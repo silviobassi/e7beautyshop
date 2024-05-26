40c4ceaa4b16aaa3a1d3c0eb50f99c24bf2519ea
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2024-05-26 16:52:50 -0300

    Refactoring Readme

--- E7BeautyShop.Schedule.Model/obj/Debug/net8.0/E7BeautyShop.Schedule.Model.AssemblyInfo.cs
+++ E7BeautyShop.Schedule.Model/obj/Debug/net8.0/E7BeautyShop.Schedule.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E7BeautyShop.Schedule.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3433aacde61b4ee43943d9f1c150969b8b10e7aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e423461f495143118bdb29094b2921892d402b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("E7BeautyShop.Schedule.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E7BeautyShop.Schedule.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
