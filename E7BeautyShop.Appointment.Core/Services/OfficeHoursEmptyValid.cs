0527116b687a7c35c2153be71931c75cdd5fc814
Silvio Bassi <silviobassi1@hotmail.com>
2024-06-21 20:57:49 -0300

    OfficeHoursEmptyValidcreating

---
+++ E7BeautyShop.Appointment.Core/Services/OfficeHoursEmptyValid.cs
@@ -0,0 +1,19 @@
+﻿using E7BeautyShop.Appointment.Core.Entities;
+
+namespace E7BeautyShop.Appointment.Core.Services;
+
+public class OfficeHoursEmptyValid : IValidator
+{
+    private readonly IReadOnlyCollection<OfficeHour> _officeHoursOrdered;
+
+    public OfficeHoursEmptyValid(IReadOnlyCollection<OfficeHour> officeHours)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(officeHours));
+        _officeHoursOrdered = officeHours.OrderBy(of => of.DateAndHour).ToList().AsReadOnly();
+    }
+
+    public bool Validate()
+    {
+        return _officeHoursOrdered.Count == 0;
+    }
+}
