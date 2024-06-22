fb9b9cff8afcf06f15cba84122b3673961932ca7
Silvio Bassi <silviobassi1@hotmail.com>
2024-06-22 12:46:33 -0300

    ScheduleHelper Created

--- E7BeautyShop.Appointment.Tests/UnitTests/Core/Services/HasAtLeastTwoItemsValidTest.cs
+++ E7BeautyShop.Appointment.Tests/UnitTests/Core/Services/HasAtLeastTwoItemsValidTest.cs
@@ -1,109 +1,47 @@
-﻿using E7BeautyShop.Appointment.Core.Entities;
-using E7BeautyShop.Appointment.Core.ObjectsValue;
-using E7BeautyShop.Appointment.Core.Services;
-
-namespace E7BeautyShop.Appointment.Tests.UnitTests.Core.Services;
+﻿namespace E7BeautyShop.Appointment.Tests.UnitTests.Core.Services;
 
 public class HasAtLeastTwoItemsValidTest
 {
     [Fact]
     public void Should_Check_HasAtLeastTwoItems_InList()
     {
-        var startAt = new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc);
-        var endAt = new DateTime(2024, 07, 18, 12, 0, 0, DateTimeKind.Utc);
+        var schedule = ScheduleTestHelper.CreateSchedule();
+        ScheduleTestHelper.AddOfficeHours(schedule,
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 0, 0, 30),
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 30, 0, 30));
 
-        ProfessionalId? professionalId = Guid.Parse("0bc3810b-f85c-4ea4-84e0-557726a65940");
+        var timeToSchedule = ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 7, 20, 0, 30);
 
-        var startWeekday = new TimeSpan(8, 0, 0);
-        var endWeekday = new TimeSpan(17, 0, 0);
-        var startWeekend = new TimeSpan(8, 0, 0);
-        var endWeekend = new TimeSpan(12, 0, 0);
-
-        Weekday weekday = (startWeekday, endWeekday);
-        Weekend weekend = (startWeekend, endWeekend);
-
-        var schedule = Schedule.Create(startAt, endAt, professionalId, weekday, weekend);
-        var officeHour1 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc), 30);
-        var officeHour2 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 30, 0, DateTimeKind.Utc), 30);
-        schedule.AddOfficeHour(officeHour1);
-        schedule.AddOfficeHour(officeHour2);
-
-        var timeToSchedule = OfficeHour.Create(new DateTime(2024, 06, 18, 7, 20, 0, DateTimeKind.Utc), 30);
-
-        var hasNoItemsValid = new HasNoItemsValid(schedule.OfficeHours);
-        var hasUniqueValid = new HasUniqueItemValid(schedule.OfficeHours, timeToSchedule);
-        var hasAtLeastTwoValid = new HasAtLeastTwoItemsValid(schedule.OfficeHours, timeToSchedule);
-        
-        Assert.False(hasNoItemsValid.Validate());
-        Assert.False(hasUniqueValid.Validate());
-        Assert.True(hasAtLeastTwoValid.Validate());
+        ScheduleTestHelper.ValidateSchedule(schedule, timeToSchedule, hasNoItemsValidExpected: false,
+            hasUniqueValidExpected: false, hasAtLeastTwoValidExpected: true);
     }
 
     [Fact]
     public void Should_Check_HasAtLeastTwoItems_InList_When_TimeToSchedule_LessThan_FirstItemList()
     {
-        var startAt = new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc);
-        var endAt = new DateTime(2024, 07, 18, 12, 0, 0, DateTimeKind.Utc);
+        var schedule = ScheduleTestHelper.CreateSchedule();
+        ScheduleTestHelper.AddOfficeHours(schedule,
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 0, 0, 30),
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 30, 0, 30));
 
-        ProfessionalId? professionalId = Guid.Parse("0bc3810b-f85c-4ea4-84e0-557726a65940");
+        var timeToSchedule = ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 7, 30, 0, 30);
 
-        var startWeekday = new TimeSpan(8, 0, 0);
-        var endWeekday = new TimeSpan(17, 0, 0);
-        var startWeekend = new TimeSpan(8, 0, 0);
-        var endWeekend = new TimeSpan(12, 0, 0);
-
-        Weekday weekday = (startWeekday, endWeekday);
-        Weekend weekend = (startWeekend, endWeekend);
-
-        var schedule = Schedule.Create(startAt, endAt, professionalId, weekday, weekend);
-        var officeHour1 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc), 30);
-        var officeHour2 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 30, 0, DateTimeKind.Utc), 30);
-        schedule.AddOfficeHour(officeHour1);
-        schedule.AddOfficeHour(officeHour2);
-
-        var timeToSchedule = OfficeHour.Create(new DateTime(2024, 06, 18, 7, 30, 0, DateTimeKind.Utc), 30);
-
-        var hasNoItemsValid = new HasNoItemsValid(schedule.OfficeHours);
-        var hasUniqueValid = new HasUniqueItemValid(schedule.OfficeHours, timeToSchedule);
-        var hasAtLeastTwoValid = new HasAtLeastTwoItemsValid(schedule.OfficeHours, timeToSchedule);
-        
-        Assert.False(hasNoItemsValid.Validate());
-        Assert.False(hasUniqueValid.Validate());
-        Assert.True(hasAtLeastTwoValid.Validate());
+        ScheduleTestHelper.ValidateSchedule(schedule, timeToSchedule, hasNoItemsValidExpected: false,
+            hasUniqueValidExpected: false, hasAtLeastTwoValidExpected: true);
     }
 
     [Fact]
-    public void Should_Check_HasAtLeastTwo_Items_InList_When_TimeToSchedule_GreaterThan_LastItemList()
+    public void Should_Check_HasAtLeastTwoItems_InList_When_TimeToSchedule_GreaterThan_LastItemList()
     {
-        var startAt = new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc);
-        var endAt = new DateTime(2024, 07, 18, 12, 0, 0, DateTimeKind.Utc);
+        var schedule = ScheduleTestHelper.CreateSchedule();
+        ScheduleTestHelper.AddOfficeHours(schedule,
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 0, 0, 30),
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 8, 30, 0, 30),
+            ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 9, 30, 0, 30));
 
-        ProfessionalId? professionalId = Guid.Parse("0bc3810b-f85c-4ea4-84e0-557726a65940");
+        var timeToSchedule = ScheduleTestHelper.CreateOfficeHour(2024, 06, 18, 9, 0, 0, 30);
 
-        var startWeekday = new TimeSpan(8, 0, 0);
-        var endWeekday = new TimeSpan(17, 0, 0);
-        var startWeekend = new TimeSpan(8, 0, 0);
-        var endWeekend = new TimeSpan(12, 0, 0);
-
-        Weekday weekday = (startWeekday, endWeekday);
-        Weekend weekend = (startWeekend, endWeekend);
-
-        var schedule = Schedule.Create(startAt, endAt, professionalId, weekday, weekend);
-        var officeHour1 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc), 30);
-        var officeHour2 = OfficeHour.Create(new DateTime(2024, 06, 18, 8, 30, 0, DateTimeKind.Utc), 30);
-        var officeHour3 = OfficeHour.Create(new DateTime(2024, 06, 18, 9, 30, 0, DateTimeKind.Utc), 30);
-        schedule.AddOfficeHour(officeHour1);
-        schedule.AddOfficeHour(officeHour2);
-        schedule.AddOfficeHour(officeHour3);
-
-        var timeToSchedule = OfficeHour.Create(new DateTime(2024, 06, 18, 9, 0, 0, DateTimeKind.Utc), 30);
-
-        var hasNoItemsValid = new HasNoItemsValid(schedule.OfficeHours);
-        var hasUniqueValid = new HasUniqueItemValid(schedule.OfficeHours, timeToSchedule);
-        var hasAtLeastTwoValid = new HasAtLeastTwoItemsValid(schedule.OfficeHours, timeToSchedule);
-        
-        Assert.False(hasNoItemsValid.Validate());
-        Assert.False(hasUniqueValid.Validate());
-        Assert.True(hasAtLeastTwoValid.Validate());
+        ScheduleTestHelper.ValidateSchedule(schedule, timeToSchedule, hasNoItemsValidExpected: false,
+            hasUniqueValidExpected: false, hasAtLeastTwoValidExpected: true);
     }
 }
---
+++ E7BeautyShop.Appointment.Tests/UnitTests/Core/Services/ScheduleHelper.cs
@@ -0,0 +1,44 @@
+﻿using E7BeautyShop.Appointment.Core.Entities;
+using E7BeautyShop.Appointment.Core.ObjectsValue;
+using E7BeautyShop.Appointment.Core.Services;
+
+namespace E7BeautyShop.Appointment.Tests.UnitTests.Core.Services;
+
+public static class ScheduleTestHelper
+{
+    public static Schedule CreateSchedule()
+    {
+        var startAt = new DateTime(2024, 06, 18, 8, 0, 0, DateTimeKind.Utc);
+        var endAt = new DateTime(2024, 07, 18, 12, 0, 0, DateTimeKind.Utc);
+        ProfessionalId? professionalId = Guid.Parse("0bc3810b-f85c-4ea4-84e0-557726a65940");
+
+        var weekday = (new TimeSpan(8, 0, 0), new TimeSpan(17, 0, 0));
+        var weekend = (new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0));
+
+        return Schedule.Create(startAt, endAt, professionalId, weekday, weekend);
+    }
+
+    public static OfficeHour CreateOfficeHour(int year, int month, int day, int hour, int minute, int second, int duration)
+    {
+        return OfficeHour.Create(new DateTime(year, month, day, hour, minute, second, DateTimeKind.Utc), duration);
+    }
+
+    public static void AddOfficeHours(Schedule schedule, params OfficeHour[] officeHours)
+    {
+        foreach (var officeHour in officeHours)
+        {
+            schedule.AddOfficeHour(officeHour);
+        }
+    }
+
+    public static void ValidateSchedule(Schedule schedule, OfficeHour timeToSchedule, bool hasNoItemsValidExpected, bool hasUniqueValidExpected, bool hasAtLeastTwoValidExpected)
+    {
+        var hasNoItemsValid = new HasNoItemsValid(schedule.OfficeHours);
+        var hasUniqueValid = new HasUniqueItemValid(schedule.OfficeHours, timeToSchedule);
+        var hasAtLeastTwoValid = new HasAtLeastTwoItemsValid(schedule.OfficeHours, timeToSchedule);
+
+        Assert.Equal(hasNoItemsValidExpected, hasNoItemsValid.Validate());
+        Assert.Equal(hasUniqueValidExpected, hasUniqueValid.Validate());
+        Assert.Equal(hasAtLeastTwoValidExpected, hasAtLeastTwoValid.Validate());
+    }
+}
